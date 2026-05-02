1d735f7329fc9014dbe9dcf14fc8254b27b8d3db
Lykkeskov <h.lykkeskov@gmail.com>
2026-05-02 16:01:53 +0200

    Skriftet til små bogtaver i databse tables så det også virker på linux (server)

--- DatabaseTables.sql
+++ DatabaseTables.sql
@@ -3,7 +3,7 @@ CREATE DATABASE cykelvaerksted;
 USE cykelvaerksted;
 
 -- Bruger tabel
-CREATE TABLE Bruger (
+CREATE TABLE bruger (
     id INT AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(100) NOT NULL,
     rolle VARCHAR(50) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE Bruger (
 );
 
 -- Cykel tabel
-CREATE TABLE Cykel (
+CREATE TABLE cykel (
     id INT AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(50),
     status VARCHAR(50),
@@ -19,35 +19,35 @@ CREATE TABLE Cykel (
 );
 
 -- Opgave tabel
-CREATE TABLE Opgave (
+CREATE TABLE opgave (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titel VARCHAR(100) NOT NULL,
     status VARCHAR(50),
     cykel_id INT NOT NULL,
     bruger_id INT,
-    FOREIGN KEY (cykel_id) REFERENCES Cykel(id),
-    FOREIGN KEY (bruger_id) REFERENCES Bruger(id)
+    FOREIGN KEY (cykel_id) REFERENCES cykel(id),
+    FOREIGN KEY (bruger_id) REFERENCES bruger(id)
 );
 
 -- Reservedele tabel
-CREATE TABLE Reservedele (
+CREATE TABLE reservedele (
     id INT AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(100) NOT NULL,
     antal INT NOT NULL
 );
 
 -- Koblingstabel    (M:M relation)
-CREATE TABLE Cykel_Reservedele (
+CREATE TABLE cykel_reservedele (
     cykel_id INT,
     reservedel_id INT,
     antal INT,
     PRIMARY KEY (cykel_id, reservedel_id),
-    FOREIGN KEY (cykel_id) REFERENCES Cykel(id),
-    FOREIGN KEY (reservedel_id) REFERENCES Reservedele(id)
+    FOREIGN KEY (cykel_id) REFERENCES cykel(id),
+    FOREIGN KEY (reservedel_id) REFERENCES reservedele(id)
 );
 
 -- Reservation table is very important for the thing yes
-CREATE TABLE Reservation (
+CREATE TABLE reservation (
     id INT AUTO_INCREMENT PRIMARY KEY,
     code VARCHAR(20) NOT NULL,
     name VARCHAR(100) NOT NULL,
